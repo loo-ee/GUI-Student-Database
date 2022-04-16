2430b128ae0c824c350efc5dc05834c1336d54c4
CoderLui <92241727+lui523202@users.noreply.github.com>
2022-04-16 15:27:50 +0800

    Relocated the whole project

--- GUI-StudentDatabase/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.louie.guistudentdatabase {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.louie.guistudentdatabase to javafx.fxml;
-    exports com.louie.guistudentdatabase;
-}
